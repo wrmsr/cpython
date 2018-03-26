ad97f4f8ef96e333ffa1a0b111e492baab32309d
wrmsr <timwilloney@gmail.com>
2018-03-26 05:00:38 -0700

    --

--- Python/ceval.c
+++ Python/ceval.c
@@ -561,6 +561,7 @@ _PyEval_EvalFrameDefault(PyFrameObject *f, int throwflag)
     PyObject *retval = NULL;            /* Return value */
     PyThreadState *tstate = PyThreadState_GET();
     PyCodeObject *co;
+    Py_LOCAL_THREAD_STATE;
 
     /* when tracing we set things up so that
 
@@ -976,6 +977,7 @@ _PyEval_EvalFrameDefault(PyFrameObject *f, int throwflag)
                 /* Other threads may run now */
 
                 take_gil(tstate);
+                Py_LOCAL_THREAD_STATE_REFRESH;
 
                 /* Check if we should make a quick exit. */
                 if (_Py_IsFinalizing() &&
