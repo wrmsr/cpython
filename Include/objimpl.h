a7e8a28d5d0b04270730d3119f239f6e8f19fadc
wrmsr <timwilloney@gmail.com>
2020-03-27 05:32:30 -0700

    --

--- Include/objimpl.h
+++ Include/objimpl.h
@@ -138,6 +138,7 @@ _PyObject_INIT(PyObject *op, PyTypeObject *typeobj)
 {
     assert(op != NULL);
     Py_TYPE(op) = typeobj;
+    // FIXME: no? pin? lazy? eager?
     if (PyType_GetFlags(typeobj) & Py_TPFLAGS_HEAPTYPE) {
         Py_TREFCNT(op)->owned.owner_id = (_Py_Freethreaded ? _Py_THREADSTATE_OWNERSHIP_BLOCK->owner_id : 0);
         Py_INCREF(typeobj);
